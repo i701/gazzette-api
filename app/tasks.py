da03a5af073166cc955f84cdaeca62a3740c86a8
i701 <idhaan701@outlook.com>
2024-08-24 16:40:19 +0500

    add celery tasks to update stale data

---
+++ app/tasks.py
@@ -0,0 +1,46 @@
+from app.utils.celery import celery
+from app.utils.helpers import iulaan_search_with_url
+from app.models.models import Result
+import json
+from tortoise.exceptions import DoesNotExist
+import asyncio
+from tortoise import Tortoise
+from decouple import config
+import time
+import random
+
+
+@celery.task
+def refresh_data():
+    loop = asyncio.get_event_loop()
+    if loop.is_closed():
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+    result = loop.run_until_complete(update_stale_results())
+    return result
+
+
+async def update_stale_results():
+    await Tortoise.init(
+        db_url=config("DATABASE_URL", cast=str),
+        modules={"models": ["app.models"]},
+    )
+
+    total_results = await Result.all().count()
+    print("TOTAL RESULTS -> ", total_results)
+    for db_result in await Result.all():
+        time.sleep(random.uniform(1, 5))
+        try:
+            actual_result = await iulaan_search_with_url(db_result.url)
+            if db_result.content != actual_result:
+                print("CONTENT CHANGED!!")
+                updated_result_obj = await Result.filter(
+                    search_key=db_result.search_key,
+                ).update(content=json.dumps(actual_result))
+                print(updated_result_obj)
+        except DoesNotExist:
+            print("Does not exist")
+        except Exception as e:
+            print(e)
+    await asyncio.sleep(1)
+    return "Database results Updated!"
---
+++ app/utils/celery.py
@@ -0,0 +1,24 @@
+from celery.app import Celery
+from decouple import config
+from datetime import timedelta
+
+redis_url = config("REDIS_URL")
+
+celery = Celery(__name__, broker=redis_url, backend=redis_url, include=["app.tasks"])
+
+
+# celery.conf.update(
+#     task_serializer="json",
+#     accept_content=["json"],
+#     result_serializer="json",
+#     timezone="UTC",
+#     enable_utc=True,
+# )
+
+celery.autodiscover_tasks(["app"])
+celery.conf.beat_schedule = {
+    "refresh-every-hour": {
+        "task": "app.tasks.refresh_data",  # "schedule": crontab(minute=0, hour="*"),  # Runs every hour
+        "schedule": timedelta(hours=1),  # Runs every 1 seconds
+    },
+}
