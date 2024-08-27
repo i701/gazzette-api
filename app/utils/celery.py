a3d89b55946714304a61589d3c4d3c5bb3a9e8cb
i701 <idhaan701@outlook.com>
2024-08-27 09:03:56 +0500

    use crontab and update gitignore

--- app/utils/celery.py
+++ app/utils/celery.py
@@ -1,6 +1,6 @@
 from celery.app import Celery
 from decouple import config
-from datetime import timedelta
+from celery.schedules import crontab
 
 redis_url = config("REDIS_URL")
 
@@ -18,9 +18,10 @@ celery = Celery(__name__, broker=redis_url, backend=redis_url, include=["app.tas
 celery.autodiscover_tasks(["app"])
 celery.conf.beat_schedule = {
     "refresh-every-hour": {
-        "task": "app.tasks.refresh_data",  # "schedule": crontab(minute=0, hour="*"),  # Runs every hour
-        "schedule": timedelta(
-            hours=config("REFRESH_TIME_HOURS", cast=int)
-        ),  # Runs every 1 seconds
+        "task": "app.tasks.refresh_data",
+        "schedule": crontab(minute=0, hour="*"),  # Runs every hour
+        # "schedule": timedelta(
+            # hours=config("REFRESH_TIME_HOURS", cast=int)
+        # ),  # Runs every 1 seconds
     },
 }
